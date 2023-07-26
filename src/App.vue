ae3deb72dde77906badaad1978a97c3b08ebe166
mvazquezma <mvm51090@gmail.com>
2023-07-26 19:54:59 +0200

    Create login vue and route

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,41 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+  import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <v-card
+    elevation="3"
+    max-width="1200"
+    class="mx-auto"
+  >
+    <v-layout>
+      <v-app-bar
+        color="blue-darken-1"
+      >
+        <template v-slot:prepend>
+          <v-btn :to="{name: 'home'}">
+            Bienes Raices - Vuefire
+          </v-btn>
+        </template>
+        
+        <template v-slot:append>
+          <v-btn :to="{name: 'home'}">
+           Inicio
+          </v-btn>
+          <v-btn :to="{name: 'login'}">
+           Iniciar sesión
+          </v-btn>
+        </template>
+        
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <RouterView />
+        </v-container>
+      </v-main>
+      
+    </v-layout>
+    
+  </v-card>
 </template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+    <h1 class="text-center text-h3 font-weight-bold my-5">Iniciar sesión</h1>
+</template>
