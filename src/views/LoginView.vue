66aeb22a2d60fc300e7e3bebb62835da16f74c91
mvazquezma <mvm51090@gmail.com>
2023-07-26 20:05:41 +0200

    Create login form with vuetify

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,5 +2,45 @@
 </script>
 
 <template>
-    <h1 class="text-center text-h3 font-weight-bold my-5">Iniciar sesión</h1>
+
+    <v-card
+        flat
+        max-width="600"
+        class="mx-auto my-10"
+    >
+        <v-card-title
+            class="text-h4 font-weight-bold"
+            tag="h3"
+        >
+            Iniciar sesión
+        </v-card-title>
+
+        <v-card-subtitle
+            class="text-h5"
+        >
+            Inicia sesión con tu cuenta
+        </v-card-subtitle>
+
+        <v-form class="mt-5">
+            <v-text-field
+                type="email"
+                label="Email"
+                bg-color="blue-grey-lighten-5"
+            />
+
+            <v-text-field
+                type="password"
+                label="Password"
+                bg-color="blue-grey-lighten-5"
+            />
+
+            <v-btn
+                block
+                color="pink-accent-3"
+            >
+                Iniciar sesión
+            </v-btn>
+        </v-form>
+    </v-card>
+
 </template>
