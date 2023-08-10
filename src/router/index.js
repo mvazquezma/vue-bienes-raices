e83825851a02462dd724348d38adea50c839c1f7
mvazquezma <mvm51090@gmail.com>
2023-08-10 20:38:40 +0200

    Allow access if user is authenticated

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useFirebaseAuth } from 'vuefire'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -39,16 +41,39 @@ const router = createRouter({
 })
 
 // Guard de navegación
-router.beforeEach((to, from, next) => {
+router.beforeEach(async(to, from, next) => {
   const requiresAuth = to.matched.some(url => url.meta.requiresAuth)
   
   if(requiresAuth) {
     // Comprobar que el usuario esté autenticado
-    
+    try {
+      await authenticateUser()
+      next()
+    } catch (error) {
+      console.error(error);
+      next({name: 'login'})
+    }
+
   } else {
     // No está protegido, mostramos la vista
     next()
   }
 })
+
+function authenticateUser() {
+  const auth = useFirebaseAuth()
+
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe()
+
+      if(user) {
+        resolve(user)
+      } else {
+        reject()
+      }
+    })
+  })
+}
 
 export default router
