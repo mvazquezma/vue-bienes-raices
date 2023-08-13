f8d8c9c1a7acdbebae75bb5873136004cce519e8
mvazquezma <mvm51090@gmail.com>
2023-08-13 12:20:50 +0200

    Create composable to upload images and install uid package

---
+++ src/composables/useImage.js
@@ -0,0 +1,18 @@
+import { ref as storageRef } from "firebase/storage"
+import { useFirebaseStorage, useStorageFile } from "vuefire"
+import { uid } from 'uid'
+
+export default function useImage() {
+
+    const storage = useFirebaseStorage()
+    const storageRefPath = storageRef(storage, `/propiedades/${uid()}`)
+
+    const {
+        url,
+        upload
+    } = useStorageFile(storageRefPath)
+    
+    return {
+
+    }
+}
