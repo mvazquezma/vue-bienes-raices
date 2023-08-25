4ebd0c971f37da63bda80c522b3617b714498c2d
mvazquezma <mvm51090@gmail.com>
2023-08-25 18:59:05 +0200

    Update property

--- src/views/admin/EditarPropiedadView.vue
+++ src/views/admin/EditarPropiedadView.vue
@@ -1,6 +1,6 @@
 <script setup>
      import { watch } from 'vue';
-    import { useRoute } from 'vue-router'
+    import { useRoute, useRouter } from 'vue-router'
     import { useFirestore, useDocument } from 'vuefire';
     import { doc, updateDoc } from 'firebase/firestore';
     import { useField, useForm } from 'vee-validate'
@@ -31,6 +31,7 @@
     const alberca = useField('alberca')
 
     const route = useRoute()
+    const router = useRouter()
 
     // Obtener la propiedad
     const db = useFirestore()
@@ -48,8 +49,19 @@
         center.value = propiedad.ubicacion
     })
 
-    const submit = handleSubmit(values => {
-        
+    const submit = handleSubmit(async values => {
+        const { imagen, ...propiedad } = values
+        if(image.value) {
+            console.log('tenemos imagen neuva');
+        } else {
+            const data = {
+                ...propiedad,
+                ubicacion: center.value
+            }
+            await updateDoc(docRef, data)
+        }
+
+        router.push({name: 'admin-propiedades'})
     })
 </script>
 
