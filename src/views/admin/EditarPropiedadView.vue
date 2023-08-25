40ee9c20803c0698cf7dab087c8ae055c4c15c50
mvazquezma <mvm51090@gmail.com>
2023-08-25 19:01:38 +0200

    Update property if there are new image

--- src/views/admin/EditarPropiedadView.vue
+++ src/views/admin/EditarPropiedadView.vue
@@ -52,7 +52,12 @@
     const submit = handleSubmit(async values => {
         const { imagen, ...propiedad } = values
         if(image.value) {
-            console.log('tenemos imagen neuva');
+            const data = {
+                ...propiedad,
+                imagen: url.value,
+                ubicacion: center.value
+            }
+            await updateDoc(docRef, data)
         } else {
             const data = {
                 ...propiedad,
