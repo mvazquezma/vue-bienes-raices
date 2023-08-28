fb1bd82310d3fc68a8559b8540e6ce45d4f7a3a0
mvazquezma <mvm51090@gmail.com>
2023-08-28 09:51:47 +0200

    Show properties like cards and the information

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,23 @@
 <script setup>
+  import usePropiedades from '../composables/usePropiedades'
+  import Propiedad from '../components/Propiedad.vue'
+
+  const { propiedadesCollection, propertyPrice } = usePropiedades()
 </script>
 
 <template>
   <main>
-    <h1 class="text-center text-h3 font-weight-bold my-5">Vue Bienes Raices</h1>
+    <h1 class="text-center text-h3 font-weight-bold my-5">Nuestras propiedades</h1>
+
+    <v-card flat class="py-10">
+      <v-row>
+        <Propiedad 
+          v-for="propiedad in propiedadesCollection"
+          :key="propiedad.id"
+          :propiedad="propiedad"
+          :price="propertyPrice"
+        />
+      </v-row>
+    </v-card>
   </main>
 </template>
