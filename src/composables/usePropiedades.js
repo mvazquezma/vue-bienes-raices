7d6a7f5f8109ad0945ded549fe7e4e1379e10079
mvazquezma <mvm51090@gmail.com>
2023-08-28 15:30:11 +0200

    Local Filter

--- src/composables/usePropiedades.js
+++ src/composables/usePropiedades.js
@@ -1,12 +1,22 @@
+import { computed, ref } from "vue"
 import { collection } from "firebase/firestore"
 import { useFirestore, useCollection } from "vuefire"
 
 export default function usePropiedades() {
+    const alberca = ref(false)
 
     const db = useFirestore()
     const propiedadesCollection = useCollection(collection(db, 'propiedades'))
 
+    const propiedadesFiltradas = computed(() => {
+        return alberca.value ?
+            propiedadesCollection.value.filter(propiedad => propiedad.alberca) :
+            propiedadesCollection.value
+    })
+
     return {
         propiedadesCollection,
+        propiedadesFiltradas,
+        alberca
     }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   import usePropiedades from '../composables/usePropiedades'
   import Propiedad from '../components/Propiedad.vue'
   import { propertyPrice } from '../helpers'
+  const { alberca, propiedadesFiltradas } = usePropiedades()
 
   const { propiedadesCollection } = usePropiedades()
 </script>
@@ -11,9 +12,14 @@
     <h1 class="text-center text-h3 font-weight-bold my-5">Nuestras propiedades</h1>
 
     <v-card flat class="py-10">
+      <v-card-title class="text-h5">
+        Búsqueda
+        <v-checkbox label="Alberca" v-model="alberca" />
+      </v-card-title>
+
       <v-row>
         <Propiedad 
-          v-for="propiedad in propiedadesCollection"
+          v-for="propiedad in propiedadesFiltradas"
           :key="propiedad.id"
           :propiedad="propiedad"
           :price="propertyPrice"
